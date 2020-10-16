cedd11c3f91a297cdd5f42d5a62fc04f0b919809
Ahmed Abdallah <ahmadpx@hotmail.com>
2020-10-16 10:21:28 +0400

    solution 2

--- problem-solving/practise.js
+++ problem-solving/practise.js
@@ -11,6 +11,9 @@
  */
 function organizingContainers(containers) {
   let possibility = "Possible";
+  const containersCount = containers.map((container) =>
+    container.reduce((sum, ball) => sum + ball, 0)
+  );
 
   for (let i = 0; i < containers.length; i++) {
     const container = containers[i];
@@ -19,37 +22,42 @@ function organizingContainers(containers) {
       const comparedContainer = containers[j];
       for (let k = 0; k < container.length; k++) {
         if (k === i || container[k] === 0) continue;
-        while (comparedContainer[i] > 0 && container[k] > 0) {
-          // fill container type
-          ++container[i];
-          --comparedContainer[i];
-          // swap equivalent balls taken by container
-          ++comparedContainer[k];
-          --container[k];
-        console.log({ container, comparedContainer });
-        }
+        const takeAll = container[k] >= comparedContainer[i];
+        const ballsToReplace = takeAll ? comparedContainer[i] : container[k];
+        container[i] += ballsToReplace;
+        comparedContainer[i] -= ballsToReplace;
+        comparedContainer[k] += ballsToReplace;
+        container[k] -= ballsToReplace;
+        if (takeAll) break;
       }
-      console.log("==");
     }
-    if (container[i] !== containers[i].reduce((sum, ball) => sum + ball, 0)) {
-      possibility = "Impossible";
-      break;
-    }
+    // if (container[i] !== containerCount) {
+    //   possibility = "Impossible";
+    //   break;
+    // }
   }
+  
+  // return containers;
 
-  return containers;
+  return containers.some(
+    (container, idx) => container[idx] !== containersCount[idx]
+  )
+    ? "Impossible"
+    : "Possible";
+
+  // return containers;
 
   return possibility;
 }
 
-console.log(
-  organizingContainers([
-    [1, 3, 1],
-    [2, 1, 2],
-    [3, 3, 3],
-  ])
-); // Impossible
-
+// console.log(
+//   organizingContainers([
+//     [1, 3, 1],
+//     [2, 1, 2],
+//     [3, 3, 3],
+//   ])
+// ); // Impossible
+//
 // console.log(
 //   organizingContainers([
 //     [0, 2, 1],
@@ -72,106 +80,107 @@ console.log(
 //   ])
 // ); // Impossible
 
-// console.log( // Possible
-//   organizingContainers([
-//     [
-//       993263231,
-//       806455183,
-//       972467746,
-//       761846174,
-//       226680660,
-//       667393859,
-//       815298761,
-//       790313908,
-//       997845516,
-//     ],
-//     [
-//       873142072,
-//       579210472,
-//       996344520,
-//       999601755,
-//       904458846,
-//       663577990,
-//       980240637,
-//       713052540,
-//       963408591,
-//     ],
-//     [
-//       551159221,
-//       873763794,
-//       752756532,
-//       798803609,
-//       670921889,
-//       995259612,
-//       981339960,
-//       763904498,
-//       499472207,
-//     ],
-//     [
-//       975980611,
-//       999974039,
-//       729219884,
-//       834636710,
-//       973988213,
-//       969888254,
-//       846967495,
-//       687204298,
-//       511348404,
-//     ],
-//     [
-//       993232608,
-//       998103218,
-//       857820670,
-//       995587240,
-//       817798750,
-//       773950980,
-//       824882180,
-//       797565274,
-//       887424441,
-//     ],
-//     [
-//       847857578,
-//       768853671,
-//       916073200,
-//       982234748,
-//       986511977,
-//       733420232,
-//       997714976,
-//       819799716,
-//       891570083,
-//     ],
-//     [
-//       733197334,
-//       985682497,
-//       612123868,
-//       971798655,
-//       999905357,
-//       710092446,
-//       997494889,
-//       683312998,
-//       850074746,
-//     ],
-//     [
-//       799093993,
-//       543648222,
-//       944524289,
-//       814951921,
-//       981087922,
-//       997222915,
-//       506561779,
-//       997697933,
-//       652807674,
-//     ],
-//     [
-//       989362549,
-//       973516812,
-//       891706714,
-//       786904549,
-//       982329176,
-//       376575034,
-//       993535504,
-//       984745483,
-//       777613376,
-//     ],
-//   ])
-// );
+console.log(
+  // Possible
+  organizingContainers([
+    [
+      993263231,
+      806455183,
+      972467746,
+      761846174,
+      226680660,
+      667393859,
+      815298761,
+      790313908,
+      997845516,
+    ],
+    [
+      873142072,
+      579210472,
+      996344520,
+      999601755,
+      904458846,
+      663577990,
+      980240637,
+      713052540,
+      963408591,
+    ],
+    [
+      551159221,
+      873763794,
+      752756532,
+      798803609,
+      670921889,
+      995259612,
+      981339960,
+      763904498,
+      499472207,
+    ],
+    [
+      975980611,
+      999974039,
+      729219884,
+      834636710,
+      973988213,
+      969888254,
+      846967495,
+      687204298,
+      511348404,
+    ],
+    [
+      993232608,
+      998103218,
+      857820670,
+      995587240,
+      817798750,
+      773950980,
+      824882180,
+      797565274,
+      887424441,
+    ],
+    [
+      847857578,
+      768853671,
+      916073200,
+      982234748,
+      986511977,
+      733420232,
+      997714976,
+      819799716,
+      891570083,
+    ],
+    [
+      733197334,
+      985682497,
+      612123868,
+      971798655,
+      999905357,
+      710092446,
+      997494889,
+      683312998,
+      850074746,
+    ],
+    [
+      799093993,
+      543648222,
+      944524289,
+      814951921,
+      981087922,
+      997222915,
+      506561779,
+      997697933,
+      652807674,
+    ],
+    [
+      989362549,
+      973516812,
+      891706714,
+      786904549,
+      982329176,
+      376575034,
+      993535504,
+      984745483,
+      777613376,
+    ],
+  ])
+);
