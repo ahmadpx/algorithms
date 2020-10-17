7ea51fd99539e7d618f83bc4ed137479c9a26c11
Ahmed Abdallah <ahmadpx@hotmail.com>
2020-10-17 15:11:22 +0400

    solution

--- problem-solving/practise.test.js
+++ problem-solving/practise.test.js
@@ -1,139 +1,88 @@
-// https://www.hackerrank.com/challenges/absolute-permutation/problem
+function bomberMan(n, grid) {
+  // 0  3
+  // 2  5
+  // 4  7
+  if (n < 2) return grid;
+  let currentBombs = [];
+  const isBomb = (row, col) => grid[row][col] === "O";
+  const isPlantingTime = (seconds) => seconds % 2 === 0;
+  const isBlastingTime = (seconds) => seconds % 2 === 1;
+  const detonate = ([row, col]) => {
+    grid[row][col] = ".";
+    if (col > 0) grid[row][col - 1] = ".";
+    if (col < grid[0].length - 1) grid[row][col + 1] = ".";
+    if (row < grid.length - 1) grid[row + 1][col] = ".";
+    if (row > 0) grid[row - 1][col] = ".";
+  };
+  const blast = (row, col) => {
+    const blastMap = {
+      [`${row}-${col}`]: true,
+    };
 
-function absolutePermutation(n, k) {
-  let result = [];
+    grid[row][col] = ".";
+    if (col > 0) blastMap[`${row}-${col - 1}`] = true;
+    if (col < grid[0].length - 1) blastMap[`${row}-${col + 1}`] = true;
+    if (row < grid.length - 1) blastMap[`${row + 1}-${col}`] = true;
+    if (row > 0) blastMap[`${row - 1}-${col}`] = true;
 
-  if (k === 0) return [...new Array(n + 1).keys()].slice(1);
+    return blastMap;
+  };
+  const plant = () => {
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid[row].length; col++) {
+        grid[row][col] = "O";
+      }
+    }
+  };
+  const getCurrentPlantedBombs = (plant = false) => {
+    let currentBombs = [];
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid[row].length; col++) {
+        if (isBomb(row, col)) {
+          currentBombs.push([row, col]);
+        }
+        if (plant) grid[row][col] = "O";
+      }
+    }
+    return currentBombs;
+  };
 
-  if (n % (2 * k) !== 0) return [-1];
+  currentBombs = getCurrentPlantedBombs();
 
-  for (let i = 0; i < n; i++) {
-    result.push(Math.floor(i / k) % 2 === 0 ? i + k + 1 : i - k + 1);
+  plant();
+
+  let seconds = 2;
+  while (seconds <= n) {
+    if (isBlastingTime(seconds)) {
+      currentBombs.forEach(detonate);
+      currentBombs = getCurrentPlantedBombs(
+        isPlantingTime(seconds + 1) && seconds + 1 < n
+      );
+      console.log(currentBombs);
+    }
+    ++seconds;
   }
 
-  return result;
+  grid.forEach((r) => console.log(JSON.stringify(r)));
+
+  return grid;
 }
 
-console.log(absolutePermutation(2, 1).join("") === [2, 1].join(""));
-console.log(absolutePermutation(3, 0).join("") === [1, 2, 3].join(""));
-console.log(absolutePermutation(3, 2).join("") === [-1].join(""));
-console.log(absolutePermutation(2, 1).join("") === [2, 1].join(""));
 console.log(
-  absolutePermutation(100, 2).join("") ===
+  bomberMan(5, [
+    [".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", "O", ".", "O", "."],
+    [".", ".", ".", ".", "O", ".", "."],
+    [".", ".", "O", ".", ".", ".", "."],
+    ["O", "O", ".", ".", ".", "O", "O"],
+    ["O", "O", ".", "O", ".", ".", "."],
+  ]).reduce((acc, curr) => acc + curr.join(""), "") ===
     [
-      3,
-      4,
-      1,
-      2,
-      7,
-      8,
-      5,
-      6,
-      11,
-      12,
-      9,
-      10,
-      15,
-      16,
-      13,
-      14,
-      19,
-      20,
-      17,
-      18,
-      23,
-      24,
-      21,
-      22,
-      27,
-      28,
-      25,
-      26,
-      31,
-      32,
-      29,
-      30,
-      35,
-      36,
-      33,
-      34,
-      39,
-      40,
-      37,
-      38,
-      43,
-      44,
-      41,
-      42,
-      47,
-      48,
-      45,
-      46,
-      51,
-      52,
-      49,
-      50,
-      55,
-      56,
-      53,
-      54,
-      59,
-      60,
-      57,
-      58,
-      63,
-      64,
-      61,
-      62,
-      67,
-      68,
-      65,
-      66,
-      71,
-      72,
-      69,
-      70,
-      75,
-      76,
-      73,
-      74,
-      79,
-      80,
-      77,
-      78,
-      83,
-      84,
-      81,
-      82,
-      87,
-      88,
-      85,
-      86,
-      91,
-      92,
-      89,
-      90,
-      95,
-      96,
-      93,
-      94,
-      99,
-      100,
-      97,
-      98,
-    ].join("")
+      [".", ".", ".", ".", ".", ".", "."],
+      [".", ".", ".", "O", ".", "O", "."],
+      [".", ".", ".", "O", "O", ".", "."],
+      [".", ".", "O", "O", "O", "O", "."],
+      ["O", "O", "O", "O", "O", "O", "O"],
+      ["O", "O", "O", "O", "O", "O", "O"],
+    ].reduce((acc, curr) => acc + curr.join(""), "")
 );
-console.log(absolutePermutation(7, 5).join("") === [-1].join(""));
-console.log(absolutePermutation(2, 1).join("") === [2, 1].join(""));
-console.log(absolutePermutation(2, 0).join("") === [1, 2].join(""));
-console.log(absolutePermutation(2, 0).join("") === [1, 2].join(""));
-console.log(absolutePermutation(1, 0).join("") === [1].join(""));
-console.log(
-  absolutePermutation(10, 5).join("") ===
-    [6, 7, 8, 9, 10, 1, 2, 3, 4, 5].join("")
-);
-console.log(
-  absolutePermutation(10, 0).join("") ===
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].join("")
-);
-console.log(absolutePermutation(6, 0).join("") === [1, 2, 3, 4, 5, 6].join(""));
